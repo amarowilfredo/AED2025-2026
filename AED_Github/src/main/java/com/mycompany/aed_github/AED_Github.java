e9dc246ccb04825243e21f999f0d091e062c4057
Wilfredo2025 <Wilfredo2025@DESKTOP-ECQEJ0I>
2025-09-21 18:16:00 +0100

    Git para programación (PRO) 2025-2026

---
+++ AED_Github/src/main/java/com/mycompany/aed_github/AED_Github.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aed_github;
+
+/**
+ *
+ * @author Wilfredo2025
+ */
+public class AED_Github {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
